72f62fc93fee6a5e9515216e6fc6dccf499436a7
DevExpressExampleBot <fakeEmail>
2025-02-19 18:14:45 +0400

    Source auto update [skip ci]

--- VB/PDFSignature/Program.vb
+++ VB/PDFSignature/Program.vb
@@ -1,7 +1,8 @@
+Imports DevExpress.Pdf
 Imports System
+Imports System.Diagnostics
 Imports System.IO
 Imports System.Security.Cryptography.X509Certificates
-Imports DevExpress.Pdf
 
 Namespace PDFSignature
 
@@ -10,11 +11,11 @@ Namespace PDFSignature
         Shared Sub Main(ByVal args As String())
             Using documentProcessor As DevExpress.Pdf.PdfDocumentProcessor = New DevExpress.Pdf.PdfDocumentProcessor()
                 documentProcessor.LoadDocument("..\..\..\Document.pdf")
-                Dim certificate As New System.Security.Cryptography.X509Certificates.X509Certificate2("..\..\SignDemo.pfx", "dxdemo")
-                Dim imageData As Byte() = System.IO.File.ReadAllBytes("..\..\..\image.emf")
+                Dim certificate As X509Certificate2 = New X509Certificate2("..\..\..\SignDemo.pfx", "dxdemo")
+                Dim imageData As Byte() = File.ReadAllBytes("..\..\..\image.emf")
                 Dim pageNumber As Integer = 1
                 Dim angleInDegrees As Integer = 45
-                Dim angleInRadians As Double = angleInDegrees * (System.Math.PI / 180)
+                Dim angleInRadians As Double = angleInDegrees * (Math.PI / 180)
                 Dim signatureBounds As DevExpress.Pdf.PdfOrientedRectangle = New DevExpress.Pdf.PdfOrientedRectangle(New DevExpress.Pdf.PdfPoint(620, 210), 250, 90, angleInRadians)
                 Dim signature As DevExpress.Pdf.PdfSignature = New DevExpress.Pdf.PdfSignature(certificate, imageData, pageNumber, signatureBounds)
                 signature.Location = "USA"
@@ -22,6 +23,8 @@ Namespace PDFSignature
                 signature.Reason = "Approved"
                 documentProcessor.SaveDocument("..\..\..\SignedDocument.pdf", New DevExpress.Pdf.PdfSaveOptions() With {.Signature = signature})
             End Using
+
+            Process.Start(New ProcessStartInfo("..\..\..\SignedDocument.pdf") With {.UseShellExecute = True})
         End Sub
     End Class
 End Namespace
